6ca318dbc64e1defda104543d84812040a64e678
Siobhan B <sohbaker@gmail.com>
2019-09-24 18:57:57 +0100

    knows that the board is full

--- src/core/Board.elm
+++ src/core/Board.elm
@@ -1,28 +1,60 @@
 module Board exposing (..)
 
 import Array exposing (..)
+import List
+import String
 
 
-grid =
+board =
+    { grid = initGrid }
+
+
+initGrid : List String
+initGrid =
     List.range 1 (gridSize * gridSize)
+        |> List.map (\x -> String.fromInt x)
 
 
+gridSize : Int
 gridSize =
     3
 
 
-cells =
-    List.map (\x -> String.fromInt x) grid
+cells : { grid : List String } -> List String
+cells boardRecord =
+    List.map (\x -> x) boardRecord.grid
 
 
-makeMove number mark =
-    set (number - 1) mark (fromList cells)
+makeMove : Int -> String -> List String -> List String
+makeMove position mark currentBoard =
+    if isValidMove position currentBoard then
+        cells <| updateBoard position mark currentBoard
+
+    else
+        currentBoard
+
+
+updateBoard : Int -> String -> List String -> { grid : List String }
+updateBoard position mark currentBoard =
+    { grid = setMark position mark currentBoard }
+
+
+setMark : Int -> String -> List String -> List String
+setMark position mark currentBoard =
+    set (position - 1) mark (fromList currentBoard)
         |> toList
 
 
-availableMoves board =
-    List.filter (\x -> x /= "X" && x /= "O") board
+availableMoves : List String -> List String
+availableMoves currentBoard =
+    List.filter (\x -> x /= "X" && x /= "O") currentBoard
 
 
-isValidMove move board =
-    List.member move (availableMoves board)
+isValidMove : Int -> List String -> Bool
+isValidMove move currentBoard =
+    List.member (String.fromInt move) (availableMoves currentBoard)
+
+
+isFull : List String -> Bool
+isFull currentBoard =
+    List.all (\x -> x == "X" || x == "O") currentBoard
--- tests/BoardTest.elm
+++ tests/BoardTest.elm
@@ -1,12 +1,33 @@
 module BoardTest exposing (..)
 
-import Board
+import Board exposing (board)
 import Expect exposing (Expectation)
+import List
 import Test exposing (..)
 
 
 boardGrid =
-    Board.cells
+    Board.cells board
+
+
+markOne =
+    "X"
+
+
+markTwo =
+    "O"
+
+
+fullBoard =
+    Board.makeMove 7 markOne <|
+        Board.makeMove 9 markTwo <|
+            Board.makeMove 6 markOne <|
+                Board.makeMove 8 markTwo <|
+                    Board.makeMove 5 markOne <|
+                        Board.makeMove 4 markTwo <|
+                            Board.makeMove 2 markOne <|
+                                Board.makeMove 3 markTwo <|
+                                    Board.makeMove 1 markOne boardGrid
 
 
 suite : Test
@@ -15,14 +36,17 @@ suite =
         [ test "board takes a move" <|
             \() ->
                 True
-                    |> Expect.equal (List.member "X" (Board.makeMove 1 "X"))
+                    |> Expect.equal (List.member "X" (Board.makeMove 1 "X" boardGrid))
         , test "board knows the available moves" <|
             \() ->
-                Expect.false "Expected position 1 is unavailable" (List.member "1" <| Board.availableMoves <| Board.makeMove 1 "X")
+                Expect.false "Exp: position 1 is unavailable" (List.member "1" <| Board.availableMoves <| Board.makeMove 1 "X" boardGrid)
         , test "board knows when a move is valid" <|
             \() ->
-                Expect.true "Expected move is valid" (Board.isValidMove "1" boardGrid)
+                Expect.true "Exp: valid move" (Board.isValidMove 1 boardGrid)
         , test "board knows when a move is not valid" <|
             \() ->
-                Expect.false "Expected move is invalid" (Board.isValidMove "2" <| Board.makeMove 2 "O")
+                Expect.false "Exp: invalid move" (Board.isValidMove 2 <| Board.makeMove 2 "O" boardGrid)
+        , test "knows that the board is full" <|
+            \() ->
+                Expect.true "Exp: board is full" (Board.isFull fullBoard)
         ]
