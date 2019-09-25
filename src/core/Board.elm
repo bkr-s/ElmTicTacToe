ae3086b211e4d0439c4a71c06f641e568ced284e
Siobhan B <sohbaker@gmail.com>
2019-09-25 09:48:24 +0100

    knows that the player with mark 'x' has won

--- src/core/Board.elm
+++ src/core/Board.elm
@@ -5,8 +5,11 @@ import List
 import String
 
 
+board : { grid : List String, winningLines : List (List Int) }
 board =
-    { grid = initGrid }
+    { grid = initGrid
+    , winningLines = winningCombos
+    }
 
 
 initGrid : List String
@@ -20,34 +23,31 @@ gridSize =
     3
 
 
-cells : { grid : List String } -> List String
-cells boardRecord =
+winningCombos : List (List Int)
+winningCombos =
+    [ [ 0, 1, 2 ]
+    , [ 3, 4, 5 ]
+    , [ 6, 7, 8 ]
+    , [ 0, 3, 6 ]
+    , [ 1, 4, 7 ]
+    , [ 2, 5, 8 ]
+    , [ 2, 4, 6 ]
+    , [ 0, 4, 8 ]
+    ]
+
+
+getGrid : { grid : List String } -> List String
+getGrid boardRecord =
     List.map (\x -> x) boardRecord.grid
 
 
-makeMove : Int -> String -> List String -> List String
-makeMove position mark currentBoard =
+marksBoardIfValidMove : Int -> String -> List String -> List String
+marksBoardIfValidMove position mark currentBoard =
     if isValidMove position currentBoard then
-        cells <| updateBoard position mark currentBoard
+        getGrid <| updateGrid position mark currentBoard
 
     else
         currentBoard
-
-
-updateBoard : Int -> String -> List String -> { grid : List String }
-updateBoard position mark currentBoard =
-    { grid = setMark position mark currentBoard }
-
-
-setMark : Int -> String -> List String -> List String
-setMark position mark currentBoard =
-    set (position - 1) mark (fromList currentBoard)
-        |> toList
-
-
-availableMoves : List String -> List String
-availableMoves currentBoard =
-    List.filter (\x -> x /= "X" && x /= "O") currentBoard
 
 
 isValidMove : Int -> List String -> Bool
@@ -55,6 +55,45 @@ isValidMove move currentBoard =
     List.member (String.fromInt move) (availableMoves currentBoard)
 
 
+markBoard : Int -> String -> List String -> List String
+markBoard position mark currentBoard =
+    set (position - 1) mark (Array.fromList currentBoard)
+        |> Array.toList
+
+
+updateGrid : Int -> String -> List String -> { grid : List String }
+updateGrid position mark currentBoard =
+    { grid = markBoard position mark currentBoard }
+
+
+availableMoves : List String -> List String
+availableMoves currentBoard =
+    List.filter (\x -> x /= "X" && x /= "O") currentBoard
+
+
 isFull : List String -> Bool
 isFull currentBoard =
     List.all (\x -> x == "X" || x == "O") currentBoard
+
+
+hasPlayerWon : String -> List String -> Bool
+hasPlayerWon mark currentBoard =
+    checkAllLines mark currentBoard
+
+
+checkAllLines : String -> List String -> Bool
+checkAllLines mark currentBoard =
+    board.winningLines
+        |> List.map (\lines -> checkSingleLine mark lines currentBoard)
+        |> List.filter ((==) True)
+        |> List.isEmpty
+        |> not
+
+
+checkSingleLine : String -> List Int -> List String -> Bool
+checkSingleLine mark lines currentBoard =
+    lines
+        |> List.map (\index -> get index (Array.fromList currentBoard) |> Maybe.withDefault "")
+        |> List.filter (\x -> x == mark)
+        |> List.length
+        |> (==) 3
--- tests/BoardTest.elm
+++ tests/BoardTest.elm
@@ -7,46 +7,57 @@ import Test exposing (..)
 
 
 boardGrid =
-    Board.cells board
+    Board.getGrid { grid = board.grid }
 
 
-markOne =
-    "X"
-
-
-markTwo =
+naught =
     "O"
 
 
+cross =
+    "X"
+
+
 fullBoard =
-    Board.makeMove 7 markOne <|
-        Board.makeMove 9 markTwo <|
-            Board.makeMove 6 markOne <|
-                Board.makeMove 8 markTwo <|
-                    Board.makeMove 5 markOne <|
-                        Board.makeMove 4 markTwo <|
-                            Board.makeMove 2 markOne <|
-                                Board.makeMove 3 markTwo <|
-                                    Board.makeMove 1 markOne boardGrid
+    Board.marksBoardIfValidMove 7 cross <|
+        Board.marksBoardIfValidMove 9 naught <|
+            Board.marksBoardIfValidMove 6 cross <|
+                Board.marksBoardIfValidMove 8 naught <|
+                    Board.marksBoardIfValidMove 5 cross <|
+                        Board.marksBoardIfValidMove 4 naught <|
+                            Board.marksBoardIfValidMove 2 cross <|
+                                Board.marksBoardIfValidMove 3 naught <|
+                                    Board.marksBoardIfValidMove 1 cross boardGrid
+
+
+boardWithXAsTheWinner =
+    Board.marksBoardIfValidMove 3 cross <|
+        Board.marksBoardIfValidMove 5 naught <|
+            Board.marksBoardIfValidMove 2 cross <|
+                Board.marksBoardIfValidMove 4 naught <|
+                    Board.marksBoardIfValidMove 1 cross boardGrid
 
 
 suite : Test
 suite =
     describe "The Board module"
-        [ test "board takes a move" <|
+        [ test "marks the board" <|
             \() ->
                 True
-                    |> Expect.equal (List.member "X" (Board.makeMove 1 "X" boardGrid))
-        , test "board knows the available moves" <|
+                    |> Expect.equal (List.member "X" (Board.marksBoardIfValidMove 1 "X" boardGrid))
+        , test "knows the available moves" <|
             \() ->
-                Expect.false "Exp: position 1 is unavailable" (List.member "1" <| Board.availableMoves <| Board.makeMove 1 "X" boardGrid)
-        , test "board knows when a move is valid" <|
+                Expect.false "Exp: position 1 is unavailable" (List.member "1" <| Board.availableMoves <| Board.marksBoardIfValidMove 1 "X" boardGrid)
+        , test "knows when a move is valid" <|
             \() ->
                 Expect.true "Exp: valid move" (Board.isValidMove 1 boardGrid)
-        , test "board knows when a move is not valid" <|
+        , test "knows when a move is not valid" <|
             \() ->
-                Expect.false "Exp: invalid move" (Board.isValidMove 2 <| Board.makeMove 2 "O" boardGrid)
+                Expect.false "Exp: invalid move" (Board.isValidMove 2 <| Board.marksBoardIfValidMove 2 "O" boardGrid)
         , test "knows that the board is full" <|
             \() ->
                 Expect.true "Exp: board is full" (Board.isFull fullBoard)
+        , test "knows that 'X' has won" <|
+            \() ->
+                Expect.true "Exp: player 'X' has won" (Board.hasPlayerWon cross boardWithXAsTheWinner)
         ]
