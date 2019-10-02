a10ff3bcef896a7a77c059996c8a9e90351a9782
bkr-s <sbaker@8thlight.com>
2019-10-02 19:56:44 +0100

    moves functions around showing or getting a player to own module

--- src/Player.elm
+++ src/Player.elm
@@ -1,5 +1,7 @@
 module Player exposing (..)
 
+import Array exposing (Array)
+
 
 type Player
     = X
@@ -7,6 +9,32 @@ type Player
     | Unclaimed
 
 
-selectMove : Int -> Int
-selectMove move =
-    move
+getOpponent : Player -> Player
+getOpponent player =
+    case player of
+        X ->
+            O
+
+        O ->
+            X
+
+        Unclaimed ->
+            Unclaimed
+
+
+showPlayer : Player -> String
+showPlayer player =
+    case player of
+        X ->
+            "X"
+
+        O ->
+            "O"
+
+        Unclaimed ->
+            ""
+
+
+value : Array String -> Int -> String
+value grid index =
+    Array.get index grid |> Maybe.withDefault ""
