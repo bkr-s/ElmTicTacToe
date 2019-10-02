8378280b9611cbd321b763e35eac56733cb073b4
bkr-s <55702723+bkr-s@users.noreply.github.com>
2019-10-02 09:58:02 +0100

    Easy Computer (#5)
    * adds an easy computer move when player is computer
    
    * formats views depending on game state
    
    * uses custom HTML file to view app
    
    * moves styling to css file
    
    * updates json and .js file following elm-live install

--- src/Main.elm
+++ src/Main.elm
@@ -4,30 +4,38 @@ import Array exposing (Array)
 import Board exposing (Board)
 import Browser
 import Dict
-import Html exposing (Attribute, Html, button, div, h1, text)
-import Html.Attributes exposing (class, id, style)
+import Html exposing (Attribute, Html, button, div, h1, h4, text)
+import Html.Attributes exposing (class, id)
 import Html.Events exposing (onClick)
 import Player exposing (Player)
 
 
 
--- MODEL2
+-- MODEL
 
 
 type alias Model =
-    { board : Board, currentPlayer : Player, otherPlayer : Player, tempBoard : Board, status : GameStatus }
+    { board : Board, currentPlayer : Player, computerPlayer : Maybe Player, status : GameStatus }
 
 
-initModel : Model
-initModel =
-    { board = Board.initBoard, currentPlayer = Player.X, otherPlayer = Player.O, tempBoard = Board.initBoard, status = NewGame }
+init : Model
+init =
+    { board = Board.initBoard, currentPlayer = Player.X, computerPlayer = Nothing, status = NewGame }
 
 
 type GameStatus
     = NewGame
-    | InProgress
+    | Playing
     | Winner Player
     | Drawn
+
+
+type Msg
+    = HumanVsHuman
+    | HumanVsEasyComputer
+    | TakeTurn Int
+    | ResetGame
+    | NoOp
 
 
 
@@ -36,16 +44,81 @@ type GameStatus
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [ centreAlign, font ] [ text "Welcome To Tic Tac Toe" ]
-        , div [ class "displayGrid", centreAlign ]
-            [ displayGrid (List.map getPlayer (Dict.values model.board))
+    div [ id "container" ]
+        [ viewWelcomeMessage model
+        , viewGameMenu model
+        , viewGameStatus model
+        , div []
+            [ viewBoard (List.map showPlayer (Dict.values model.board))
             ]
         ]
 
 
-getPlayer : Player -> String
-getPlayer player =
+viewWelcomeMessage : Model -> Html Msg
+viewWelcomeMessage model =
+    let
+        welcomeMessage =
+            case model.status of
+                Playing ->
+                    text ""
+
+                Winner _ ->
+                    text ""
+
+                Drawn ->
+                    text ""
+
+                NewGame ->
+                    text "Welcome To Tic Tac Toe"
+    in
+    h1 [ class "message", id "welcome-message" ] [ welcomeMessage ]
+
+
+viewGameMenu : Model -> Html Msg
+viewGameMenu model =
+    let
+        newGameButtons =
+            case model.status of
+                Playing ->
+                    text ""
+
+                _ ->
+                    div []
+                        [ text "Start new game: "
+                        , buttonNewGame "2 Player" HumanVsHuman
+                        , buttonNewGame "1 Player (Easy)" HumanVsEasyComputer
+                        ]
+    in
+    div [ class "message", id "game-menu" ] [ newGameButtons ]
+
+
+buttonNewGame : String -> Msg -> Html Msg
+buttonNewGame buttonText msg =
+    button [ class "new-game-button", onClick msg ] [ text buttonText ]
+
+
+viewGameStatus : Model -> Html Msg
+viewGameStatus model =
+    let
+        gameStatusMessage =
+            case model.status of
+                Playing ->
+                    [ text "" ]
+
+                Winner _ ->
+                    [ text (showPlayer model.currentPlayer), text " wins!!" ]
+
+                Drawn ->
+                    [ text "It's a tie!" ]
+
+                NewGame ->
+                    [ text "" ]
+    in
+    h4 [ class "message", id "game-status-message" ] gameStatusMessage
+
+
+showPlayer : Player -> String
+showPlayer player =
     case player of
         Player.X ->
             "X"
@@ -57,78 +130,85 @@ getPlayer player =
             ""
 
 
-displayGrid : List String -> Html Msg
-displayGrid grid =
+viewBoard : List String -> Html Msg
+viewBoard grid =
     Array.fromList grid
-        |> formatBoxes
+        |> formatCells
 
 
-formatBoxes : Array String -> Html Msg
-formatBoxes allBoxes =
-    div []
-        [ div [ padRow ]
-            [ button [ onClick (TakeTurn 1) ] [ text (Array.get 0 allBoxes |> Maybe.withDefault "") ]
-            , button [ onClick (TakeTurn 2) ] [ text (Array.get 1 allBoxes |> Maybe.withDefault "") ]
-            , button [ onClick (TakeTurn 3) ] [ text (Array.get 2 allBoxes |> Maybe.withDefault "") ]
+formatCells : Array String -> Html Msg
+formatCells allCells =
+    div [ class "board" ]
+        [ div [ class "board-row" ]
+            [ button [ onClick (TakeTurn 1) ] [ text (currentPlayerValue allCells 0) ]
+            , button [ onClick (TakeTurn 2) ] [ text (currentPlayerValue allCells 1) ]
+            , button [ onClick (TakeTurn 3) ] [ text (currentPlayerValue allCells 2) ]
             ]
-        , div [ padRow ]
-            [ button [ onClick (TakeTurn 4) ] [ text (Array.get 3 allBoxes |> Maybe.withDefault "") ]
-            , button [ onClick (TakeTurn 5) ] [ text (Array.get 4 allBoxes |> Maybe.withDefault "") ]
-            , button [ onClick (TakeTurn 6) ] [ text (Array.get 5 allBoxes |> Maybe.withDefault "") ]
+        , div [ class "board-row" ]
+            [ button [ onClick (TakeTurn 4) ] [ text (currentPlayerValue allCells 3) ]
+            , button [ onClick (TakeTurn 5) ] [ text (currentPlayerValue allCells 4) ]
+            , button [ onClick (TakeTurn 6) ] [ text (currentPlayerValue allCells 5) ]
             ]
-        , div [ padRow ]
-            [ button [ onClick (TakeTurn 7) ] [ text (Array.get 6 allBoxes |> Maybe.withDefault "") ]
-            , button [ onClick (TakeTurn 8) ] [ text (Array.get 7 allBoxes |> Maybe.withDefault "") ]
-            , button [ onClick (TakeTurn 9) ] [ text (Array.get 8 allBoxes |> Maybe.withDefault "") ]
+        , div [ class "board-row" ]
+            [ button [ onClick (TakeTurn 7) ] [ text (currentPlayerValue allCells 6) ]
+            , button [ onClick (TakeTurn 8) ] [ text (currentPlayerValue allCells 7) ]
+            , button [ onClick (TakeTurn 9) ] [ text (currentPlayerValue allCells 8) ]
             ]
         ]
 
 
-padRow : Attribute a
-padRow =
-    style "padding" "0.5em"
-
-
-centreAlign : Attribute a
-centreAlign =
-    style "text-align" "center"
-
-
-font : Attribute a
-font =
-    style "font-family" "sans-serif"
+currentPlayerValue : Array String -> Int -> String
+currentPlayerValue grid index =
+    Array.get index grid |> Maybe.withDefault ""
 
 
 
 -- UPDATE
 
 
-type Msg
-    = TakeTurn Int
-    | ResetGame
-    | NoOp
-
-
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        TakeTurn position ->
-            if (model.status == InProgress || model.status == NewGame) && Board.isValidMove position model.board then
-                { model | board = Board.markBoard position model.currentPlayer model.board }
-                    |> checkStatus
+        HumanVsHuman ->
+            { init | status = Playing }
 
-            else
-                model
+        HumanVsEasyComputer ->
+            { init | computerPlayer = Just Player.O, status = Playing }
+
+        TakeTurn position ->
+            let
+                newModel =
+                    if canContinueGame model && Board.isValidMove position model.board then
+                        { model | board = Board.markBoard position model.currentPlayer model.board }
+                            |> checkGameStatus
+
+                    else
+                        model
+
+                addComputerMoveIfRelevant =
+                    if isComputerTurn newModel && canContinueGame newModel then
+                        getComputerMove newModel
+                            |> checkGameStatus
+
+                    else
+                        newModel
+            in
+            addComputerMoveIfRelevant
 
         ResetGame ->
-            initModel
+            init
 
         NoOp ->
             model
 
 
-checkStatus : Model -> Model
-checkStatus ({ board, currentPlayer, otherPlayer } as model) =
+canContinueGame : Model -> Bool
+canContinueGame model =
+    model.status == Playing || model.status == NewGame
+
+
+checkGameStatus : Model -> Model
+checkGameStatus ({ board, currentPlayer } as model) =
     let
         isWinningPlayer =
             Board.hasPlayerWon currentPlayer board
@@ -136,21 +216,68 @@ checkStatus ({ board, currentPlayer, otherPlayer } as model) =
         isDrawnGame =
             Board.isATie board
 
-        ( newStatus, newCurrent, newOther ) =
+        ( newStatus, newCurrentPlayer ) =
             case ( isWinningPlayer, isDrawnGame ) of
                 ( True, False ) ->
-                    ( Winner currentPlayer, currentPlayer, otherPlayer )
+                    ( Winner currentPlayer, currentPlayer )
 
                 ( False, True ) ->
-                    ( Drawn, currentPlayer, otherPlayer )
+                    ( Drawn, currentPlayer )
 
                 ( False, False ) ->
-                    ( InProgress, otherPlayer, currentPlayer )
+                    ( Playing, getOpponent currentPlayer )
 
                 ( _, _ ) ->
-                    ( NewGame, otherPlayer, currentPlayer )
+                    ( NewGame, currentPlayer )
     in
-    { model | status = newStatus, currentPlayer = newCurrent, otherPlayer = newOther }
+    { model | status = newStatus, currentPlayer = newCurrentPlayer }
+
+
+getOpponent : Player -> Player
+getOpponent player =
+    case player of
+        Player.X ->
+            Player.O
+
+        Player.O ->
+            Player.X
+
+        Player.Unclaimed ->
+            Player.Unclaimed
+
+
+isComputerTurn : Model -> Bool
+isComputerTurn ({ board, currentPlayer, computerPlayer } as model) =
+    if isComputerPlayer currentPlayer model && (model.status == Playing) then
+        True
+
+    else
+        False
+
+
+isComputerPlayer : Player -> Model -> Bool
+isComputerPlayer currentPlayer model =
+    case model.computerPlayer of
+        Just computerPlayer ->
+            currentPlayer == computerPlayer
+
+        _ ->
+            False
+
+
+getComputerMove : Model -> Model
+getComputerMove model =
+    case computerSelectsRandomMove (Board.availableMoves model.board) of
+        Just move ->
+            { model | board = Board.markBoard move model.currentPlayer model.board }
+
+        Nothing ->
+            model
+
+
+computerSelectsRandomMove : List Int -> Maybe Int
+computerSelectsRandomMove availablePositions =
+    List.head availablePositions
 
 
 
@@ -160,7 +287,7 @@ checkStatus ({ board, currentPlayer, otherPlayer } as model) =
 main : Program () Model Msg
 main =
     Browser.sandbox
-        { init = initModel
+        { init = init
         , view = view
         , update = update
         }
