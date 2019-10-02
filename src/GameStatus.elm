ae26016e54c1b07dd600618bf2a10b23fed99186
bkr-s <sbaker@8thlight.com>
2019-10-02 19:57:51 +0100

    creates game status module. does note create cyclical import

--- src/GameStatus.elm
+++ src/GameStatus.elm
@@ -1,1 +1,42 @@
-module Status exposing (..)
+module GameStatus exposing (..)
+
+import Board exposing (Board)
+import Player exposing (Player)
+
+
+type GameStatus
+    = NewGame
+    | Playing
+    | Winner Player
+    | Drawn
+
+
+canContinueGame : GameStatus -> Bool
+canContinueGame status =
+    status == Playing || status == NewGame
+
+
+checkGameStatus : Board -> Player -> ( GameStatus, Player )
+checkGameStatus board currentPlayer =
+    let
+        isWinningPlayer =
+            Board.hasPlayerWon currentPlayer board
+
+        isDrawnGame =
+            Board.isATie board
+
+        ( newStatus, newCurrentPlayer ) =
+            case ( isWinningPlayer, isDrawnGame ) of
+                ( True, False ) ->
+                    ( Winner currentPlayer, currentPlayer )
+
+                ( False, True ) ->
+                    ( Drawn, currentPlayer )
+
+                ( False, False ) ->
+                    ( Playing, Player.getOpponent currentPlayer )
+
+                ( _, _ ) ->
+                    ( NewGame, currentPlayer )
+    in
+    ( newStatus, newCurrentPlayer )
